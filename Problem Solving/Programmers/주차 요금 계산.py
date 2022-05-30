f1b2716f7cced3b0331afb486c96647afa5bde41
LAPTOP-8FKT8S9V\hyeon <uc07102hi@gmail.com>
2022-05-31 04:23:59 +0900

    주차요금계산 구현

--- Problem Solving/Programmers/주차 요금 계산.py
+++ Problem Solving/Programmers/주차 요금 계산.py
@@ -25,10 +25,9 @@ def solution(fees, records):
             total_fee = basic_fee
             total_fee += ceil((used_time - basic_time) / unit_time) * unit_fee
             fee_infos[car_number] = total_fee
-    
-    print([i[1] for i in list(sorted(fee_infos.items()))])
 
-    return
+    return [i[1] for i in list(sorted(fee_infos.items()))]
+    
 
 fees = [180, 5000, 10, 600]
 records = ["05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", "07:59 5961 OUT", "07:59 0148 IN", "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"]
