581a0edd80a37e93bf35c921ef14478714da56ed
LAPTOP-8FKT8S9V\hyeon <uc07102hi@gmail.com>
2022-08-20 02:32:28 +0900

    9251 LCS

---
+++ Problem Solving/Baekjoon/9251_LCS.py
@@ -0,0 +1,23 @@
+# 알고리즘 참고자료
+# https://velog.io/@emplam27/%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-%EA%B7%B8%EB%A6%BC%EC%9C%BC%EB%A1%9C-%EC%95%8C%EC%95%84%EB%B3%B4%EB%8A%94-LCS-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-Longest-Common-Substring%EC%99%80-Longest-Common-Subsequence
+
+a = input()
+b = input()
+answer = 0
+
+lcs = [
+    [0 for _ in range(len(b) + 1)]
+    for _ in range(len(a) + 1)
+]
+
+for i in range(1, len(a) + 1):
+    for j in range(1, len(b) + 1):
+        if a[i-1] == b[j-1]:
+            lcs[i][j] = lcs[i-1][j-1] + 1
+        else:
+            lcs[i][j] = max(lcs[i-1][j], lcs[i][j-1])
+        
+        if answer < lcs[i][j]:
+            answer = lcs[i][j]
+
+print(answer)
