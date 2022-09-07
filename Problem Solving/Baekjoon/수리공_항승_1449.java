6b79bd63d5a2116a788ce04bd71993c5f5b4630b
LAPTOP-8FKT8S9V\hyeon <uc07102hi@gmail.com>
2022-09-08 05:14:57 +0900

    1439 수리공 항승

---
+++ Problem Solving/Baekjoon/수리공_항승_1449.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class 수리공_항승_1449 {
+    static int n, l;
+    static int[] arr;
+    static int answer;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        l = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        int left = arr[0];
+        for (int i = 1; i < n; i++) {
+            if (arr[i] - left >= l) {
+                left = arr[i];
+                answer++;
+            }
+        }
+
+        System.out.println(++answer);
+    }
+}
