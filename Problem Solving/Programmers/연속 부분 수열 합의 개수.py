1061e5f8fb2fe16429bee61a3ff98d61d524fb77
LAPTOP-8FKT8S9V\hyeon <uc07102hi@gmail.com>
2022-11-22 13:49:21 +0900

    연속 부분 수열 합의 개수

---
+++ Problem Solving/Programmers/연속 부분 수열 합의 개수.py
@@ -0,0 +1,16 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/131701
+
+def solution(elements):
+    answer = set()
+
+    for i in range(len(elements)):
+        tmp = 0
+        for j in range(len(elements)):
+            tmp += elements[(i + j) % len(elements)]
+            answer.add(tmp)
+
+    return len(answer)
+
+
+elements = [7, 8, 1, 1, 4]
+print(solution(elements))
